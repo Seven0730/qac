708e11dbe6f243b339f31c2014f8893c1a8bf8df
chenhui <1037823529@qq.com>
2024-10-11 17:56:02 +0800

    git commit -m "QAC-36 finish comment CRUD except notification"

---
+++ event/src/main/java/com/team12/event/comment/CommentController.java
@@ -0,0 +1,56 @@
+package com.team12.event.comment;
+
+
+import com.team12.clients.comment.dto.CommentModifyRequest;
+import com.team12.clients.comment.dto.CommentSendRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@Slf4j
+@RestController
+@RequestMapping("api/v1/comment")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    public CommentController(CommentService commentService) {this.commentService = commentService;}
+
+    @PostMapping("/sendComment")
+    public void sendComment(@RequestBody CommentSendRequest commentSendRequest) {
+        commentService.commentSend(commentSendRequest);
+        log.info("Sending comment {}", commentSendRequest);
+    }
+
+
+    @PutMapping("/modifyComment/{id}")
+    public ResponseEntity<String> modifyComment(@PathVariable UUID id, @RequestBody CommentModifyRequest request) {
+        log.info("Modifying comment with ID: {}, New Content: {}", id, request.content());
+        boolean modified = commentService.commentModify(id, request);
+
+        if (modified) {
+            return ResponseEntity.ok("Comment modified successfully");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Comment not found");
+        }
+    }
+
+
+    @DeleteMapping("/deleteComment/{id}")
+    public ResponseEntity<String> deleteComment(@PathVariable UUID id) {
+        boolean deleted = commentService.commentDelete(id);
+
+        if (deleted) {
+            return ResponseEntity.ok("Comment deleted successfully");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Comment not found");
+        }
+    }
+
+
+
+
+}
