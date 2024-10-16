0a0357b22333ceeff72c5366a9eda2a106a18707
Seven0730 <hanhai0730@gmail.com>
2024-10-16 15:34:43 +0800

    Update NotificationRepository.java

--- event/src/main/java/com/team12/event/notification/repository/NotificationRepository.java
+++ event/src/main/java/com/team12/event/notification/repository/NotificationRepository.java
@@ -3,5 +3,7 @@ package com.team12.event.notification.repository;
 import com.team12.event.notification.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NotificationRepository extends JpaRepository<Notification, Integer> {
+import java.util.UUID;
+
+public interface NotificationRepository extends JpaRepository<Notification, UUID> {
 }
