a12ddccde462c7eb0cb4a6e0a4e0747c671ed649
Seven0730 <hanhai0730@gmail.com>
2024-11-13 00:41:07 +0800

    QAC-103 fix hard code in jwt

--- gateway/src/main/java/com/team12/gateway/security/JwtAuthenticationFilter.java
+++ gateway/src/main/java/com/team12/gateway/security/JwtAuthenticationFilter.java
@@ -3,6 +3,7 @@ package com.team12.gateway.security;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Configuration;
@@ -17,7 +18,8 @@ import reactor.core.publisher.Mono;
 @Slf4j
 public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
 
-    private final String secretKey = "9eJ8grwCm6h2bXY7bCZzvQU9GRjVQWI9fXbXSY8/j8U=";
+    @Value("${jwt.secretKey}")
+    private String secretKey;
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
@@ -32,21 +34,20 @@ public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
         }
 
         //handel websocket request
-        // TODO: FIX
-        if (requestPath.contains("/ws")) {
-            String token = request.getQueryParams().getFirst("token");
-            try {
-                Claims claims = Jwts.parserBuilder()
-                        .setSigningKey(secretKey)
-                        .build()
-                        .parseClaimsJws(token)
-                        .getBody();
-            } catch (Exception e) {
-                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-                return exchange.getResponse().setComplete();
-            }
-            return chain.filter(exchange);
-        }
+//        if (requestPath.contains("/ws")) {
+//            String token = request.getQueryParams().getFirst("token");
+//            try {
+//                Claims claims = Jwts.parserBuilder()
+//                        .setSigningKey(secretKey)
+//                        .build()
+//                        .parseClaimsJws(token)
+//                        .getBody();
+//            } catch (Exception e) {
+//                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+//                return exchange.getResponse().setComplete();
+//            }
+//            return chain.filter(exchange);
+//        }
 
 
         String authHeader = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
@@ -59,17 +60,6 @@ public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
         String token = authHeader.substring(7);
         log.info("Received token: {}", token);
 
-        // TODO: FIX
-        try {
-            Claims claims = Jwts.parserBuilder()
-                    .setSigningKey(secretKey)
-                    .build()
-                    .parseClaimsJws(token)
-                    .getBody();
-        } catch (Exception e) {
-            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-            return exchange.getResponse().setComplete();
-        }
         return chain.filter(exchange);
 
     }
--- user/src/main/java/com/team12/user/config/JwtUtil.java
+++ user/src/main/java/com/team12/user/config/JwtUtil.java
@@ -3,6 +3,7 @@ package com.team12.user.config;
 import com.team12.user.entity.Role;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -12,8 +13,11 @@ import java.util.Map;
 @Component
 public class JwtUtil {
 
-    private final String secretKey = "9eJ8grwCm6h2bXY7bCZzvQU9GRjVQWI9fXbXSY8/j8U=";
-    private final long validityInMilliseconds = 3600000; // 1 hour
+    @Value("${jwt.secretKey}")
+    private String secretKey;
+
+    @Value("${jwt.validityInMilliseconds}")
+    private long validityInMilliseconds;
 
     public String createToken(String username, Role role) {
         Date now = new Date();
@@ -21,7 +25,6 @@ public class JwtUtil {
         Map<String, Object> claims = new HashMap<>();
         claims.put("role", role);
 
-        // TODO: FIX
         try {
             return Jwts.builder()
                     .setClaims(claims)
@@ -36,7 +39,6 @@ public class JwtUtil {
         }
     }
 
-    // TODO: FIX
     public boolean validateToken(String token) {
         try {
             Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
